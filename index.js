f76d3f8d27453b0b61edcb204b1f59788ff962a4
Justin Zeng <hi.justinzeng@gmail.com>
2021-03-07 14:18:56 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const { Client } = require("pg");
+
+app.get("/", async (req, res) => {
+  let requests = [];
+
+  const client = new Client({ database: 'requestbindb' });
+  await client.connect();
+  
+  const result = await client.query('SELECT * FROM requests;');
+
+  result.rows.forEach(request => {
+    requests.push(request);
+  });
+
+  await client.end();
+
+  res.status(200).render('requests', {requests});
+});
+
+app.post("/", async (req, res) => {
+  let headers = JSON.stringify(req.rawHeaders);
+
+  const client = new Client({ database: 'requestbindb' });
+  await client.connect();
+  await client.query(`INSERT INTO requests (headers) VALUES ('${headers}')`);
+  await client.end();
+
+  res.status(200);
+});
+
+
+app.listen(3000, () => {
+  console.log(`Example app listening at port:3000`);
+});
